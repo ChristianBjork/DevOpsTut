2f40c1753c7010b7fca14a5fb0fceaf48dd62bd3
Tifferks <LP2296@gmail.com>
2020-03-22 16:20:41 +0100

    Changed values in the hopes it triggers my pipeline

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "playbook" };
+            return new string[] { "Ihope", "ThisWorks" };
         }
 
         // GET api/values/5
