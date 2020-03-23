d8f41c165fb16bd9b72abd3a4e78cf0c9b697672
Tifferks <LP2296@gmail.com>
2020-03-23 14:06:52 +0100

    For Demo

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "Ihope", "ThisWorks" };
+            return new string[] { "Yes this", "Fucking works" };
         }
 
         // GET api/values/5
